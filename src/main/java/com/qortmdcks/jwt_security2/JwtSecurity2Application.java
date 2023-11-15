81e5800c7530bfd05d56dce4f28cc6d3a1fd9c4d
baekseungchan1117 <bsc7386@gmail.com>
2023-11-15 15:45:35 +0900

    docker setting, db connect

---
+++ src/main/java/com/qortmdcks/jwt_security2/JwtSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.jwt_security2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurity2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/qortmdcks/jwt_security2/JwtSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.jwt_security2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
